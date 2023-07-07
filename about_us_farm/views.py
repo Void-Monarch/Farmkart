4768f5a299c504b831fe4fa4cb734596c408cb4d
VoidMonarch <102967317+Void-Monarch@users.noreply.github.com>
2023-07-07 16:52:30 +0530

    Delete about_us_farm directory

--- about_us_farm/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def about_us(request):
-    
-    return render(request, 'aboutus/aboutus.html',{})
