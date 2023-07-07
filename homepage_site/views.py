867fa1240b918ea0376a47a275d0c92e6b9aca22
VoidMonarch <102967317+Void-Monarch@users.noreply.github.com>
2023-07-07 16:53:02 +0530

    Delete homepage_site directory

--- homepage_site/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def homepage_site(request):
-    cart_cost = 2000
-    cart_items = 1
-    return render(request, 'homepage_site/homepage_site.html',{'cart_cost':cart_cost, 'cart_items':cart_items})
